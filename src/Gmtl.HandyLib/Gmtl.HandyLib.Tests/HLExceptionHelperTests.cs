92855606341f62cae5262a01dca3c6e292c6a291
Pawel Klimczyk <pawel@klimczyk.pl>
2015-11-05 20:37:51 +0100

    Added Exception XML serialization (ToXmlString)

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HLExceptionHelperTests.cs
@@ -0,0 +1,152 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLExceptionHelperTests.cs" project="Gmtl.HandyLib.Tests" date="2015-11-02 07:30">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLExceptionHelperTests
+    {
+        private string serializedException;
+
+        [Test]
+        public void ExceptionHelper_providedException_shouldSerializeIt()
+        {
+            //Arrange
+            string exceptionMessage = "TestMessage";
+            Exception exc = new Exception(exceptionMessage);
+
+            //Act
+            serializedException = exc.ToXmlString(true);
+
+            //Assert
+            var xml = deserializeException(serializedException);
+
+            Assert.That(xml.Name.LocalName, Is.EqualTo("System.Exception"));
+            Assert.That(xml.Nodes().Count(), Is.EqualTo(1));
+
+            XElement messageNode = xml.FirstNode as XElement;
+            Assert.That(messageNode, Is.Not.Null);
+            Assert.That(messageNode.Value, Is.EqualTo(exceptionMessage));
+        }
+
+        [Test]
+        public void ExceptionHelper_providedExceptionWith1StackTrace_shouldSerializeIt()
+        {
+            //Arrange
+            string exceptionMessage = "TestMessage";
+            Exception exc = CreateExceptionWithStackTrace(exceptionMessage);
+
+            //Act
+            serializedException = exc.ToXmlString(true);
+
+            //Assert
+            var xml = deserializeException(serializedException);
+
+            List<XElement> xElements = xml.Nodes().OfType<XElement>().ToList();
+            Assert.That(xml.Name.LocalName, Is.EqualTo("System.Exception"));
+            Assert.That(xElements.Count, Is.EqualTo(2));
+
+            Assert.That(xElements.Where(n => n.Name.LocalName == "StackTrace").Count(), Is.EqualTo(1));
+            Assert.That(xElements.Where(n => n.Name.LocalName == "StackTrace").FirstOrDefault().Nodes().Count(), Is.EqualTo(1));
+
+            XElement messageNode = xElements[0];
+            Assert.That(messageNode, Is.Not.Null);
+            Assert.That(messageNode.Value, Is.EqualTo(exceptionMessage));
+        }
+
+        [Test]
+        public void ExceptionHelper_providedExceptionWith5StackTrace_shouldSerializeIt()
+        {
+            //Arrange
+            string exceptionMessage = "TestMessage";
+            Exception exc = CreateExceptionWithStackTrace(exceptionMessage, 4 /* starting with 0 :) */);
+
+            //Act
+            serializedException = exc.ToXmlString(true);
+
+            //Assert
+            var xml = deserializeException(serializedException);
+
+            List<XElement> xElements = xml.Nodes().OfType<XElement>().ToList();
+            Assert.That(xml.Name.LocalName, Is.EqualTo("System.Exception"));
+            Assert.That(xElements.Count, Is.EqualTo(2));
+
+            Assert.That(xElements.Where(n => n.Name.LocalName == "StackTrace").Count(), Is.EqualTo(1));
+            Assert.That(xElements.Where(n => n.Name.LocalName == "StackTrace").FirstOrDefault().Nodes().Count(), Is.EqualTo(5));
+
+            XElement messageNode = xElements[0];
+            Assert.That(messageNode, Is.Not.Null);
+            Assert.That(messageNode.Value, Is.EqualTo(exceptionMessage));
+        }
+
+        [Test]
+        public void ExceptionHelper_providedExceptionAndInnerException_shouldSerializeIt()
+        {
+            //Arrange
+            string exceptionMessage = "TestMessage";
+            string innerExceptionMessage = "TestInnerMessage";
+            Exception exc = new Exception(exceptionMessage, new Exception(innerExceptionMessage));
+
+            //Act
+            serializedException = exc.ToXmlString(true);
+
+            //Assert
+            var xml = deserializeException(serializedException);
+            
+            List<XElement> xElements = xml.Nodes().OfType<XElement>().ToList();
+            Assert.That(xml.Name.LocalName, Is.EqualTo("System.Exception"));
+            Assert.That(xElements.Count, Is.EqualTo(2));
+
+            Assert.That(xElements.Count(n => n.Name.LocalName == "System.Exception"), Is.EqualTo(1));
+            Assert.That(xElements[0].Value, Is.EqualTo(exceptionMessage));
+            Assert.That(xElements[1].Value, Is.EqualTo(innerExceptionMessage));
+        }
+        
+        private Exception CreateExceptionWithStackTrace(string exceptionMessage, int depth = 0)
+        {
+            Action<string, int> drillDownStackTrace = null;
+
+            drillDownStackTrace = (message, step) =>
+            {
+                if (step == 0)
+                {
+                    throw new Exception(exceptionMessage);
+                }
+
+                drillDownStackTrace(message, step - 1);
+            };
+
+            try
+            {
+                if (depth == 0)
+                {
+                    throw new Exception(exceptionMessage);
+                }
+
+                drillDownStackTrace(exceptionMessage, depth - 1);
+
+                return new Exception("Never reaches this point");
+            }
+            catch (Exception exc)
+            {
+                return exc;
+            }
+        }
+
+        private static XElement deserializeException(string exception)
+        {
+            return XElement.Parse(exception);
+
+        }
+    }
+}
--- src/Gmtl.HandyLib/Gmtl.HandyLib/HLExceptionHelper.cs
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLExceptionHelper.cs
@@ -3,10 +3,46 @@
 // 
 // </copyright>
 // -------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Xml.Linq;
+
 namespace Gmtl.HandyLib
 {
     public static class HLExceptionHelper
     {
-        //TODO to string 
+        public static string ToXmlString(this Exception exception, bool includeStackTrace)
+        {
+            XElement rootElement = AddException(exception, includeStackTrace);
+
+            return rootElement.ToString();
+        }
+
+        private static XElement AddException(Exception exception, bool includeStackTrace)
+        {
+            //TODO handle exception.Data
+
+            var rootElement = new XElement(exception.GetType().ToString());
+            rootElement.Add(new XElement("Message", exception.Message));
+            
+            if (includeStackTrace && !String.IsNullOrWhiteSpace(exception.StackTrace))
+            {
+                XElement stackTraceXElement = new XElement("StackTrace");
+
+                foreach (var stackFrame in exception.StackTrace.Split('\n'))
+                {
+                    stackTraceXElement.Add(new XElement("Frame", stackFrame.Trim()));
+                }
+
+                rootElement.Add(stackTraceXElement);
+            }
+            
+            if (exception.InnerException != null)
+            {
+                rootElement.Add(AddException(exception.InnerException, includeStackTrace));
+            }
+
+            return rootElement;
+        }
     }
 }
