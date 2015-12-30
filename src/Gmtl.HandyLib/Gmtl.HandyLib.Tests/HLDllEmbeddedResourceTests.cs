f99fe812c4b672334b96deb5046033d11aece427
Pawel Klimczyk <pawel@klimczyk.pl>
2015-12-30 10:44:53 +0100

    HLDllEmbeddedResource added

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HLDllEmbeddedResourceTests.cs
@@ -0,0 +1,44 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="DllEmbeddedResourceTests.cs" project="Gmtl.HandyLib.Tests" date="2015-12-30 10:33">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLDllEmbeddedResourceTests
+    {
+        [Test]
+        public void DllEmbeddedResource_ShouldReatTextResourceFromCurrentAssembly()
+        {
+            //Assert
+            string path = "Resources.EmbeddedResourceTextFile.txt";
+            string expectedContent = "Test 123";
+
+            //Act
+            string content = HLDllEmbeddedResource.GetTextResource(path);
+
+            //Assert
+            StringAssert.IsMatch(expectedContent, content);
+        }
+
+        [Test]
+        public void DllEmbeddedResource_ShouldReatTextResourceFromProvidedAssembly()
+        {
+            //Assert
+            string path = "Resources.EmbeddedResourceTextFile.txt";
+            string expectedContent = "Test 123";
+            Assembly assembly = GetType().Assembly;
+
+            //Act
+            string content = HLDllEmbeddedResource.GetTextResource(path, assembly);
+
+            //Assert
+            StringAssert.IsMatch(expectedContent, content);
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLDllEmbeddedResource.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Gmtl.HandyLib
+{
+    public class HLDllEmbeddedResource
+    {
+        /// <summary>
+        /// Return content of the file
+        /// </summary>
+        /// <param name="resourceName"> path to the file included as embedded resource. IMPORTANT: replace '\' with '.' eg. Scripts\run.bat => Scripts.run.bat</param>
+        /// <param name="assembly">Assembly with resource</param>
+        /// <returns>content of the file</returns>
+        public static string GetTextResource(string resourceName, Assembly assembly)
+        {
+            using (Stream stream = assembly.GetManifestResourceStream(assembly.GetName().Name + "." + resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Return content of the file in calling assembly
+        /// </summary>
+        /// <param name="resourceName"> path to the file included as embedded resource. IMPORTANT: replace '\' with '.' eg. Scripts\run.bat => Scripts.run.bat</param>
+        /// <returns>content of the file</returns>
+        public static string GetTextResource(string resourceName)
+        {
+            var assembly = Assembly.GetCallingAssembly();
+
+            return GetTextResource(resourceName, assembly);
+        }
+    }
+}
