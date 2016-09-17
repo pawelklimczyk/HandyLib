c4ad3656faacae9b63448274200cfda8cfd9bbc3
Pawel Klimczyk <pawel@klimczyk.pl>
2016-09-17 12:52:10 +0200

    Added HLBase64 functions

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HlBase64Tests.cs
@@ -0,0 +1,27 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLBase64Tests.cs" project="Gmtl.HandyLib.Tests" date="2016-09-17 12:47">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLBase64Tests
+    {
+        [TestCase("sample test", "c2FtcGxlIHRlc3Q=")]
+        [TestCase("1234567890", "MTIzNDU2Nzg5MA==")]
+        public void HLBase64_providedSampleText_shouldEncodeIt(string input, string expectedOutput)
+        {
+            Assert.That(HLBase64.Encode(input), Is.EqualTo(expectedOutput));
+        }
+
+        public void HLBase64_shouldEncodeAndDecodeText()
+        {
+            string text = "Sample text for testing.";
+            Assert.That(HLBase64.Decode(HLBase64.Encode(text)), Is.EqualTo(text));
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLBase64.cs
@@ -0,0 +1,23 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLBase64.cs" project="Gmtl.HandyLib" date="2016-09-17 12:46:53">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+namespace Gmtl.HandyLib
+{
+    public static class HLBase64
+    {
+        public static string Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Decode(string encodedText)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(encodedText);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes, 0, base64EncodedBytes.Length);
+        } 
+    }
+}
