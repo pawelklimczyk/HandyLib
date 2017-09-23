5568cf378c76ca3509134995e28ae16cb887ef83
Pawel Klimczyk <pawel@klimczyk.pl>
2017-09-23 08:28:04 +0200

    Added XML documentation

--- src/Gmtl.HandyLib/Gmtl.HandyLib/HLCrypter.cs
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLCrypter.cs
@@ -16,7 +16,7 @@ namespace Gmtl.HandyLib
     /// </summary>
     public class HLCrypter
     {
-        static byte[] entropy = Encoding.Unicode.GetBytes("GMTL 2017 default entropy key");
+        static byte[] entropyKey = Encoding.Unicode.GetBytes("GMTL 2017 default entropy key");
 
         /// <summary>
         /// Set seed for encryption and decryption
@@ -24,27 +24,39 @@ namespace Gmtl.HandyLib
         /// <param name="seed">new seed</param>
         public static void SetEntropy(string seed)
         {
-            entropy = Encoding.Unicode.GetBytes(seed);
+            entropyKey = Encoding.Unicode.GetBytes(seed);
         }
 
+        /// <summary>
+        /// Encrypt string
+        /// </summary>
+        /// <param name="plainText">text to encrypt</param>
+        /// <returns>encrypted text using entropy seed</returns>
         public static string EncryptString(string plainText)
         {
             SecureString input = ToSecureString(plainText);
             byte[] encryptedData = ProtectedData.Protect(
                 Encoding.Unicode.GetBytes(ToInsecureString(input)),
-                entropy,
+                entropyKey,
                 DataProtectionScope.CurrentUser);
+
             return Convert.ToBase64String(encryptedData);
         }
 
+        /// <summary>
+        /// Decrypt string
+        /// </summary>
+        /// <param name="encryptedText">text to decrypt</param>
+        /// <returns>decrypted text using entropy seed</returns>
         public static string DecryptString(string encryptedText)
         {
             try
             {
                 byte[] decryptedData = ProtectedData.Unprotect(
                     Convert.FromBase64String(encryptedText),
-                    entropy,
+                    entropyKey,
                     DataProtectionScope.CurrentUser);
+
                 return Encoding.Unicode.GetString(decryptedData);
             }
             catch
