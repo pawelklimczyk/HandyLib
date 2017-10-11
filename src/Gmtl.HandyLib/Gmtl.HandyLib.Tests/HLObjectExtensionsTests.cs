5e0e15159070740a0cbad15b1effeb071dddad4e
Pawel Klimczyk <pawel@klimczyk.pl>
2017-10-11 14:25:14 +0200

    Added HLObjectExtensions

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HLObjectExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Gmtl.HandyLib.Extensions;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLObjectExtensionsTests
+    {
+        [Test]
+        public void ShouldListAllPropertiesAndTheirValues()
+        {
+            //Arrange
+            TestClass obj = new TestClass {IntValue = 123, StringValue = "strVal"};
+
+            //Act
+            string result = obj.PropertyList();
+
+            //Assert
+            StringAssert.Contains("123", result);
+            StringAssert.Contains("IntValue", result);
+            StringAssert.Contains("strVal", result);
+            StringAssert.Contains("StringValue", result);
+        }
+
+        class TestClass
+        {
+            public int IntValue { get; set; }
+            public string StringValue { get; set; }
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Extensions/HLObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Gmtl.HandyLib.Extensions
+{
+    /// <summary>
+    /// Usefull extensions for System.Object
+    /// </summary>
+    public static class HLObjectExtensions
+    {
+        /// <summary>
+        /// List all properties and their values 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>List of all properies and their values</returns>
+        public static string PropertyList(this object obj)
+        {
+            var props = obj.GetType().GetProperties();
+            var sb = new StringBuilder();
+
+            foreach (var p in props)
+            {
+                sb.AppendLine(p.Name + ": " + p.GetValue(obj, null));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
