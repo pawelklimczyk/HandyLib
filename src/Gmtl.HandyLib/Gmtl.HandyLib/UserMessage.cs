10d5649229db731a78db395da75f9d717a61c412
Pawel Klimczyk <pawel@klimczyk.pl>
2019-03-28 07:46:11 +0100

    Added UserMessage class

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/UserMessage.cs
@@ -0,0 +1,53 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="UserMessage.cs" project="Gmtl.HandyLib" date="2019-03-28 07:36">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+
+namespace Gmtl.HandyLib
+{
+    /// <summary>
+    /// Provides a container for standard UI user messages
+    /// </summary>
+    public class UserMessage
+    {
+        public string Title { get; private set; }
+        public string Message { get; private set; }
+        public UserMessageType Type { get; private set; }
+
+        public static UserMessage CreateInfo(string title = "", string message = "")
+        {
+            return Create(title, message, UserMessageType.Info);
+        }
+
+        public static UserMessage CreateWarning(string title = "", string message = "")
+        {
+            return Create(title, message, UserMessageType.Warning);
+        }
+
+        public static UserMessage CreateError(string title = "", string message = "")
+        {
+            return Create(title, message, UserMessageType.Error);
+        }
+
+        public static UserMessage Create(string title = "", string message = "",
+            UserMessageType type = UserMessageType.Info)
+        {
+            return new UserMessage
+            {
+                Title = title,
+                Message = message,
+                Type = type
+            };
+        }
+    }
+
+    public enum UserMessageType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+}
