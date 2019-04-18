c749f47e3e2cd8aa6870ba94f5e0bcfb777cf320
Pawel Klimczyk <pawel@klimczyk.pl>
2019-04-18 14:58:32 +0200

    Added SoftwareMetrics

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HLSoftwareMetricsTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLSoftwareMetricsTests
+    {
+        [Test]
+        public void HLSoftwareMetrics_shouldReturnMetrics()
+        {
+            Assert.IsNotEmpty(HLSoftwareMetrics.Title);
+            Assert.IsNotNull(HLSoftwareMetrics.Title);
+
+            Assert.IsNotEmpty(HLSoftwareMetrics.Copyright);
+            Assert.IsNotNull(HLSoftwareMetrics.Copyright);
+
+            Assert.IsNotEmpty(HLSoftwareMetrics.Description);
+            Assert.IsNotNull(HLSoftwareMetrics.Description);
+
+            Assert.IsNotEmpty(HLSoftwareMetrics.Version);
+            Assert.IsNotNull(HLSoftwareMetrics.Version);
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLSoftwareMetrics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Gmtl.HandyLib
+{
+    public class HLSoftwareMetrics
+    {
+        public static string Title => GetAssemblyAttribute<AssemblyTitleAttribute>(a => a.Title);
+        public static string Copyright => GetAssemblyAttribute<AssemblyCopyrightAttribute>(a => a.Copyright);
+        public static string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public static string Description => GetAssemblyAttribute<AssemblyDescriptionAttribute>(a => a.Description);
+
+        private static string GetAssemblyAttribute<T>(Func<T, string> value)
+            where T : Attribute
+        {
+            T attribute = (T)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(T));
+            return value.Invoke(attribute);
+        }
+    }
+}
