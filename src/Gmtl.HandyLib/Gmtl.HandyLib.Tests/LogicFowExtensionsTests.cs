1711bbbb70d28989fba506e8037ce28942b1659d
Pawel Klimczyk <pawel@klimczyk.pl>
2020-01-10 09:20:30 +0100

    Added HLLogicFlowExtension

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/LogicFowExtensionsTests.cs
@@ -0,0 +1,80 @@
+﻿using Gmtl.HandyLib.Extensions;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class LogicFowExtensionsTests
+    {
+        private bool DummyAction(bool returnValue)
+        {
+            return returnValue;
+        }
+
+        [Test]
+        public void TestFalseWithNoState()
+        {
+            bool trueCalled = false, falseCalled = false, alwaysCalled = false;
+            var result = DummyAction(false);
+
+            result
+                .True(() => { trueCalled = true; })
+                .False(() => { falseCalled = true; })
+                .Always(() => { alwaysCalled = true; });
+
+            Assert.IsTrue(falseCalled);
+            Assert.IsFalse(trueCalled);
+            Assert.IsTrue(alwaysCalled);
+        }
+
+        [Test]
+        public void TestFalseWithState()
+        {
+            bool trueCalled = false, falseCalled = false, alwaysCalled = false;
+            var result = DummyAction(false);
+            var state = new object();
+
+            result
+                .True(state, s => { trueCalled = true; })
+                .False(state, s => { falseCalled = true; })
+                .Always(state, s => { alwaysCalled = true; });
+
+            Assert.IsTrue(falseCalled);
+            Assert.IsFalse(trueCalled);
+            Assert.IsTrue(alwaysCalled);
+        }
+
+        [Test]
+        public void TestTrueWithNoState()
+        {
+            bool trueCalled = false, falseCalled = false, alwaysCalled = false;
+            var result = DummyAction(true);
+
+            result
+                .True(() => { trueCalled = true; })
+                .False(() => { falseCalled = true; })
+                .Always(() => { alwaysCalled = true; });
+
+            Assert.IsTrue(trueCalled);
+            Assert.IsFalse(falseCalled);
+            Assert.IsTrue(alwaysCalled);
+        }
+
+        [Test]
+        public void TestTrueWithState()
+        {
+            bool trueCalled = false, falseCalled = false, alwaysCalled = false;
+            var result = DummyAction(true);
+            var state = new object();
+
+            result
+                .True(state, s => { trueCalled = true; })
+                .False(state, s => { falseCalled = true; })
+                .Always(state, s => { alwaysCalled = true; });
+
+            Assert.IsTrue(trueCalled);
+            Assert.IsFalse(falseCalled);
+            Assert.IsTrue(alwaysCalled);
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Extensions/HLLogicFLowExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Gmtl.HandyLib.Extensions
+{
+    public static class HLLogicFlowExtensions
+    {
+        //bool result = Operation(state);
+        //if (result)
+        //{
+        //    ActionWithTrue(state);
+        //}
+        //else
+        //{
+        //    ActionWithFalse(state);
+        //}
+        //
+        //ActionAlways(state);
+
+        //bool result = Operation(false);
+        //result
+        //.True(state, s => { ActionWithTrue(s); })
+        //.False(state, s => { ActionWithFalse(s); })
+        //.Always(state, s => { ActionAlways(s); });
+
+        public static bool True(this bool flowFlag, Action action)
+        {
+            if (flowFlag)
+                action();
+
+            return flowFlag;
+        }
+
+        public static bool False(this bool flowFlag, Action action)
+        {
+            if (!flowFlag)
+                action();
+            
+            return flowFlag;
+        }
+
+        public static bool Always(this bool flowFlag, Action action)
+        {
+            action();
+            
+            return flowFlag;
+        }
+
+        public static bool True<T>(this bool flowFlag, T obj, Action<T> action)
+        {
+            if (flowFlag)
+                action(obj);
+
+            return flowFlag;
+        }
+
+        public static bool False<T>(this bool flowFlag, T obj, Action<T> action)
+        {
+            if (!flowFlag)
+                action(obj);
+
+            return flowFlag;
+        }
+
+        public static bool Always<T>(this bool flowFlag, T obj, Action<T> action)
+        {
+            action(obj);
+
+            return flowFlag;
+        }
+    }
+}
