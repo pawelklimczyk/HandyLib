5855111bfb3fe02e5841f153866a56580f5e7f0b
Pawel Klimczyk <pawel@klimczyk.pl>
2020-01-19 18:17:36 +0100

    Added DomainModelBase

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Models/DomainModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gmtl.HandyLib.Models
+{
+    public class DomainModelBase
+    {
+        protected bool SetNonEmptyStringValue(string input, ref string result)
+        {
+            if (!String.IsNullOrWhiteSpace(input))
+            {
+                result = input.Trim();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
