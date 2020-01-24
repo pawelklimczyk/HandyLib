bce50b5cab092def24bc96b92c87db5be220b62b
Pawel Klimczyk <pawel@klimczyk.pl>
2020-01-24 09:51:39 +0100

    Added HLEmailValidator + tests

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/Validators/HLEmailValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLEmailValidatorTests.cs" project="Gmtl.HandyLib.Tests" date="2020-01-24 18:29">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using Gmtl.HandyLib.Validators;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests.Validators
+{
+    [TestFixture]
+    public class HLEmailValidatorTests
+    {
+        [TestCase("test@test.com")]
+        public void HLEmailValidator_shouldBeValidEmail(string input)
+        {
+            Assert.IsTrue(HLEmailValidator.IsValidEmail(input));
+        }
+
+        [TestCase("test")]
+        [TestCase("test@test")]
+        public void HLEmailValidator_shouldBeInvalidEmail(string input)
+        {
+            Assert.IsFalse(HLEmailValidator.IsValidEmail(input));
+        }
+    }
+}
