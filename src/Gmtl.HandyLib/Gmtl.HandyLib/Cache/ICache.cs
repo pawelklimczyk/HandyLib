92394c7cc89e16e41ec83092b6be9d71202bb4e4
Mikolaj <macioszczyk.mikolaj@gmail.com>
2021-03-02 12:55:57 +0100

    Extract ICache interface

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Cache/ICache.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace Gmtl.HandyLib.Cache
+{
+    public interface ICache<TKey, TData>
+    {
+        Dictionary<TKey, TData> GetAll();
+
+        List<TData> GetList();
+
+        HLListPage<TData> FindBy<TOrder>(Func<TData, bool> filterFunc, Func<TData, TOrder> orderFunc = null,
+            OrderDirection orderDirection = OrderDirection.Ascending, int page = 1, int pageSize = 10);
+
+         TData this[TKey key] { get; }
+
+         TData Get(TKey key);
+
+         TData GetOrDefault(TKey key);
+
+         void Delete(TKey key);
+
+         void InsertOrUpdate(TKey key, TData newItemData);
+
+         void UpdateCache(TKey key, TData newItemData, CacheUpdateType updatedType);
+    }
+}
