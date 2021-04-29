0a4f76ddaac05c17ec7cb6cfcc34e2ce373b7530
Pawel Klimczyk <pawel@klimczyk.pl>
2021-04-29 08:35:53 +0200

    Added Enum.GetDescription() method

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/Extensions/HLEnumTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Gmtl.HandyLib.Extensions;
+
+namespace Gmtl.HandyLib.Tests.Extensions
+{
+    [TestFixture]
+    public class HLEnumTests
+    {
+
+        [TestCase(TestEnum.Value1, "Value 1")]
+        [TestCase(TestEnum.Value100, "Value 100")]
+        [TestCase(TestEnum.ValueWithoutDescription, "ValueWithoutDescription")]
+        public void HLEnumTests_providedEnumDescription_shouldBeValid(TestEnum val, string expectedOutput)
+        {
+            string actualOutput = val.GetDescription();
+            Assert.AreEqual(actualOutput, expectedOutput);
+        }
+    }
+
+    public enum TestEnum
+    {
+        [System.ComponentModel.Description("Value 1")]
+        Value1 = 1,
+        [System.ComponentModel.Description("Value 100")]
+        Value100 = 100,
+        ValueWithoutDescription = -1
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Extensions/HLEnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Gmtl.HandyLib.Extensions
+{
+    /// <summary>
+    /// Extensions for Enum
+    /// </summary>
+    public static class HLEnumExtensions
+    {
+        /// <summary>
+        /// Return Description attribute value
+        /// </summary>
+        public static string GetDescription(this Enum value)
+        {
+            return
+                value
+                    .GetType()
+                    .GetMember(value.ToString())
+                    .FirstOrDefault()?
+                    .GetCustomAttribute<DescriptionAttribute>()?
+                    .Description 
+                    ?? value.ToString();
+        }
+    }
+}
