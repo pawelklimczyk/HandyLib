935fadf3bdeb0ccdd8428b32536f6e96af73da55
Pawel Klimczyk <pawel@klimczyk.pl>
2021-06-09 10:09:08 +0200

    Added HLPhoneValidator

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/Validators/HLPhoneValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLPhoneValidatorTests.cs" project="Gmtl.HandyLib.Tests" date="2021-06-02 18:29">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using Gmtl.HandyLib.Validators;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests.Validators
+{
+    [TestFixture]
+    public class HLPhoneValidatorTests
+    {
+        [TestCase("500 50505053")]
+        [TestCase("5 00 505 0 5053")]
+        [TestCase("5 00   505 0   50 53")]
+        [TestCase("500 45 34 31")]
+        public void HLPhoneValidator_shouldBeValidPhoneNumber(string input)
+        {
+            Assert.IsTrue(HLPhoneValidator.IsValidPhoneNumber(input));
+        }
+
+        [TestCase("test")]
+        [TestCase("test@test.com")]
+        [TestCase("2134TRT")]
+        [TestCase("56666999")]
+        public void HLPhoneValidator_shouldBeInvalidPhoneNumber(string input)
+        {
+            Assert.IsFalse(HLPhoneValidator.IsValidPhoneNumber(input));
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Validators/HLPhoneValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gmtl.HandyLib.Validators
+{
+    public static class HLPhoneValidator
+    {
+        /// <summary>
+        /// Test if provided string is valid phone number
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsValidPhoneNumber(this string input)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                    return false;
+
+                input = input.Replace(" ", String.Empty);
+                char[] arr = input.ToCharArray();
+
+                foreach (var c in input.ToCharArray())
+                    if (!char.IsDigit(c))
+                        return false;
+
+                if (input.Length < 9)
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
