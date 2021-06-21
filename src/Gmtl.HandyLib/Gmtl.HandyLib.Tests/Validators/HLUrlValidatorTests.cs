13b398cc4390d9dae370b79010ee542a36ffdced
Pawel Klimczyk <pawel@klimczyk.pl>
2021-06-21 09:52:44 +0200

    Added HLUrlValidator

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/Validators/HLUrlValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLPhoneValidatorTests.cs" project="Gmtl.HandyLib.Tests" date="2021-06-02 18:29">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using Gmtl.HandyLib.Validators;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests.Validators
+{
+    [TestFixture]
+    public class HLUrlValidatorTests
+    {
+        [TestCase("http://onet.pl")]
+        [TestCase("https://onet.pl")]
+        [TestCase("https://aplikuj.hrlink.pl/aplikacja/Doradca-Finansowy-w-punkcie-kredytowym/43237-131481-33-19b-1647.html")]
+        public void HLUrlValidator_shouldBeUrl(string input)
+        {
+            Assert.IsTrue(HLUrlValidator.IsValidUrl(input));
+        }
+
+        [TestCase("test")]
+        [TestCase("test@test.com")]
+        [TestCase("http/onet.pl")]
+        [TestCase("aplikuj.hrlink.pl/aplikacja/Doradca-Finansowy-w-punkcie-kredytowym/43237-131481-33-19b-1647.html")]
+        public void HLUrlValidator_shouldBeInvalidUrl(string input)
+        {
+            Assert.IsFalse(HLUrlValidator.IsValidUrl(input));
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Validators/HLUrlValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gmtl.HandyLib.Validators
+{
+    /// <summary>
+    /// Validator for internet urls
+    /// </summary>
+    public static class HLUrlValidator
+    {
+        /// <summary>
+        /// Validates if provided string is valid url
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool IsValidUrl(string url)
+        {
+            Uri outUri;
+            return Uri.TryCreate(url, UriKind.Absolute, out outUri)
+                   && (outUri.Scheme == Uri.UriSchemeHttp || outUri.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
