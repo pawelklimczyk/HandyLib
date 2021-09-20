96fb2d94e4652907986b952c0cfdf5f623cde755
Pawel Klimczyk <pawel@klimczyk.pl>
2021-09-20 10:55:09 +0200

    Added HLStopWatch

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HLStopWatchTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.Threading;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLStopWatchTests
+    {
+        [Test]
+        public void ShouldUseDefaultKey()
+        {
+            HLStopWatch.Start();
+            Thread.Sleep(500);
+            long elapsed = HLStopWatch.Stop();
+
+            Assert.Greater(elapsed, 0);
+            Assert.Less(elapsed, 600);
+        }
+
+        [Test]
+        public void ShouldUseSpecifiedKey()
+        {
+            string returnedKey = HLStopWatch.Start("key123");
+            Thread.Sleep(500);
+            long elapsed = HLStopWatch.Stop(returnedKey);
+
+            Assert.AreEqual(returnedKey, "key123");
+            Assert.Greater(elapsed, 0);
+            Assert.Less(elapsed, 600);
+        }
+
+        [Test]
+        public void ShouldRunTwoWatchersWithDifferentKeys()
+        {
+            string returnedKey1 = HLStopWatch.Start("key123");
+            string returnedKey2 = HLStopWatch.Start("key456");
+            Assert.AreNotEqual(returnedKey1, returnedKey2);
+
+            Thread.Sleep(500);
+            long elapsed1 = HLStopWatch.Stop(returnedKey1);
+
+            Thread.Sleep(500);
+            long elapsed2 = HLStopWatch.Stop(returnedKey2);
+
+            Assert.Greater(elapsed1, 0);
+            Assert.Less(elapsed1, 600);
+            Assert.Greater(elapsed2, 500);
+            Assert.Less(elapsed2, 1100);
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLStopWatch.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Gmtl.HandyLib
+{
+    /// <summary>
+    /// Helper for efficient handling Stopwatch class
+    /// </summary>
+    public static class HLStopWatch
+    {
+        private static Dictionary<string, Stopwatch> watchers = new Dictionary<string, Stopwatch>();
+        private static object locker = new object();
+        private static string defaultKey = HLRandomizer.RandomString.Next(10, 15);
+
+        /// <summary>
+        /// Starts the watched with provided or default key
+        /// </summary>
+        public static string Start(string key = null)
+        {
+            if (String.IsNullOrWhiteSpace(key))
+            {
+                key = defaultKey;
+            }
+
+            lock (locker)
+            {
+                if (watchers.ContainsKey(key))
+                {
+                    throw new Exception($"StopWatch already exists. Clean it with Clean({key}).");
+                }
+
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                watchers.Add(key, watch);
+            }
+
+            return key;
+        }
+
+        /// <summary>
+        /// Stops the watcher and return elapsed milliseconds
+        /// </summary>
+        public static long Stop(string key = null)
+        {
+            if (String.IsNullOrWhiteSpace(key))
+            {
+                key = defaultKey;
+            }
+
+            lock (locker)
+            {
+                if (watchers.ContainsKey(key))
+                {
+                    Stopwatch watch = new Stopwatch();
+                    long elapsed = watchers[key].ElapsedMilliseconds;
+                    CleanNoLock(key);
+                    
+                    return elapsed;
+                }
+                else
+                {
+                    throw new Exception($"Watcher with key: {key} does not exists.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns how many milliseconds elapsed since start
+        /// </summary>
+        public static long Elapsed(string key = null)
+        {
+            if (String.IsNullOrWhiteSpace(key))
+            {
+                key = defaultKey;
+            }
+
+            lock (locker)
+            {
+                if (watchers.ContainsKey(key))
+                {
+                    return watchers[key].ElapsedMilliseconds;
+                }
+                else
+                {
+                    throw new Exception($"Watcher with key: {key} does not exists.");
+                }
+            }
+        }
+
+        public static void Clean(string key)
+        {
+            lock (locker)
+            {
+                CleanNoLock(key);
+            }
+        }
+
+        private static void CleanNoLock(string key)
+        {
+            if (watchers.ContainsKey(key))
+            {
+                Stopwatch watch = watchers[key];
+                if (watch != null)
+                {
+                    watch.Stop();
+                }
+                watchers.Remove(key);
+            }
+        }
+    }
+}
