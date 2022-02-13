8fc9daf71b8e91ff33d47357c2030079e4309c99
Pawel Klimczyk <pawel@klimczyk.pl>
2022-02-13 15:00:26 +0100

    Added HLTasks with PrimitiveSleep method

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLTasks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gmtl.HandyLib
+{
+    /// <summary>
+    /// System.Threading.Tasks extensions
+    /// </summary>
+    public class HLTasks
+    {
+        /// <summary>
+        /// Sleep for tasks with cancellation token support
+        /// </summary>
+        /// <param name="minutes">sleep in minutes</param>
+        /// <param name="tokens">external tokens</param>
+        /// <returns></returns>
+        public static async Task PrimitiveSleep(int minutes, params CancellationToken[] tokens)
+        {
+            try
+            {
+                using (CancellationTokenSource linkedCts =
+                    CancellationTokenSource.CreateLinkedTokenSource(tokens))
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(minutes), linkedCts.Token);
+                }
+            }
+            catch { }
+        }
+    }
+}
