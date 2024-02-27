d5d556a027d65ca244d06548af9d676bdf3ac9c4
Pawel Klimczyk <pawel@klimczyk.pl>
2024-02-27 08:10:30 +0100

    Added HLStringValidator.ContainsWebsiteUrl

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/Validators/HLStringValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Gmtl.HandyLib.Validators;
+using NUnit.Framework;
+
+namespace Gmtl.HandyLib.Tests.Validators
+{
+    [TestFixture]
+    public class HLStringValidatorTests
+    {
+        [TestCase("Just a string without a link", ExpectedResult = false)]
+        [TestCase("Just a string without a link. ok ", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("https://", ExpectedResult = false)]
+        [TestCase("https://.", ExpectedResult = false)]
+        public bool ShouldNotContainWebsiteAddress(string input)
+        {
+            return input.ContainsWebsiteUrl();
+        }
+
+        [TestCase("[MalformedLink]example.com)", ExpectedResult = true)]
+        [TestCase("example.com", ExpectedResult = true)]
+        [TestCase("https://www.example.com", ExpectedResult = true)]
+        [TestCase("http://example.com", ExpectedResult = true)]
+        [TestCase("https://subdomain.example.com", ExpectedResult = true)]
+        [TestCase("[Link](https://www.example.com)", ExpectedResult = true)]
+        [TestCase("This text contains a link: https://www.example.com in the middle.", ExpectedResult = true)]
+        [TestCase("This text contains a link with subfolder: https://www.example.com/path/test.php in the middle.", ExpectedResult = true)]
+        public bool ShouldContainWebsiteAddress(string input)
+        {
+            return input.ContainsWebsiteUrl();
+        }
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Validators/HLStringValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Gmtl.HandyLib.Validators
+{
+    public static class HLStringValidator
+    {
+        // private static string _pattern = @"(?<Protocol>https?://)?(?<Domain>[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,}))|(?<Link>\[([^\]]+)\]\((https?://[^\)]+)\))";
+        private static string _pattern = @"/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g";
+        private static Regex _regex = new Regex(_pattern, RegexOptions.Compiled);
+
+        /// <summary>
+        /// Check if input contains website addresses
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool ContainsWebsiteUrl(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
+            MatchCollection matches = _regex.Matches(input);
+
+            return matches.Count > 0;
+
+        }
+    }
+}
