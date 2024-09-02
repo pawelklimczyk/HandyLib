fe9bbdf40729f4f3becafc3ad760b3c21969ccbd
Pawel Klimczyk <pawel@klimczyk.pl>
2024-09-02 18:34:52 +0200

    Added OperationResultExtensions

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/Operations/OperationResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Gmtl.HandyLib.Operations
+{
+    /// <summary>
+    /// OperationResult Extensions
+    /// </summary>
+    public static class OperationResultExtensions
+    {
+        public static OperationResult<T> ToOperationResult<T>(this T item)
+        {
+            return OperationResult<T>.Success(item);
+        }
+    }
+}
