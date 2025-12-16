1197f0a0d6c253b9c03c3e62f6e41f491cb22456
Pawel Klimczyk <pawel@klimczyk.pl>
2025-12-16 14:21:04 +0100

    Added HLAssert

---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib.Tests/HLAssertTests.cs
@@ -0,0 +1,111 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLPhoneValidatorTests.cs" project="Gmtl.HandyLib.Tests" date="2025-12-15 18:29">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+
+using NUnit.Framework;
+using System;
+
+namespace Gmtl.HandyLib.Tests
+{
+    [TestFixture]
+    public class HLAssertTests
+    {
+        #region String overload tests
+
+        [Test]
+        public void AssertArgument_String_ParamNameNull_ThrowsArgumentNullExceptionForParamName()
+        {
+            // paramName is null -> method should throw ArgumentNullException for the paramName parameter itself
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument("value", null));
+            Assert.AreEqual("paramName", ex.ParamName);
+        }
+
+        [Test]
+        public void AssertArgument_String_ArgumentNull_ThrowsArgumentNullException_WithProvidedParamName()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((string)null, "myParam"));
+            Assert.AreEqual("myParam", ex.ParamName);
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("\t")]
+        [TestCase("\n")]
+        public void AssertArgument_String_EmptyOrWhitespace_ThrowsArgumentException_WithProvidedParamName(string input)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => HLAssert.AssertArgument(input, "p"));
+            Assert.AreEqual("p", ex.ParamName);
+            StringAssert.Contains("empty or whitespace", ex.Message.ToLowerInvariant());
+        }
+
+        [Test]
+        public void AssertArgument_String_Valid_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => HLAssert.AssertArgument("valid", "p"));
+        }
+
+        [Test]
+        public void AssertArgument_String_EmptyParamName_AllowsEmptyParamNameAndUsesItInException()
+        {
+            // paramName is empty string - method only checks for null, so empty is allowed.
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((string)null, string.Empty));
+            Assert.AreEqual(string.Empty, ex.ParamName);
+        }
+
+        #endregion
+
+        #region Object overload tests
+
+        [Test]
+        public void AssertArgument_Object_ParamNameNull_ThrowsArgumentNullExceptionForParamName()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((object)null, null));
+            Assert.AreEqual("paramName", ex.ParamName);
+        }
+
+        [Test]
+        public void AssertArgument_Object_ArgumentNull_ThrowsArgumentNullException_WithProvidedParamName()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((object)null, "objParam"));
+            Assert.AreEqual("objParam", ex.ParamName);
+        }
+
+        [Test]
+        public void AssertArgument_Object_NotNull_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => HLAssert.AssertArgument(new object(), "objParam"));
+        }
+
+        [Test]
+        public void AssertArgument_Object_EmptyParamName_AllowsEmptyParamNameAndUsesItInException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((object)null, string.Empty));
+            Assert.AreEqual(string.Empty, ex.ParamName);
+        }
+
+        #endregion
+
+        #region Overload resolution and ambiguity checks
+
+        [Test]
+        public void AssertArgument_NullCastToString_InvokesStringOverload()
+        {
+            // Explicit cast to string should call the string overload and therefore throw with provided param name
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((string)null, "sParam"));
+            Assert.AreEqual("sParam", ex.ParamName);
+        }
+
+        [Test]
+        public void AssertArgument_NullCastToObject_InvokesObjectOverload()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => HLAssert.AssertArgument((object)null, "oParam"));
+            Assert.AreEqual("oParam", ex.ParamName);
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Gmtl.HandyLib/Gmtl.HandyLib/HLAssert.cs
@@ -0,0 +1,39 @@
+﻿// -------------------------------------------------------------------------------------------------------------------
+// <copyright company="Gemotial" file="HLPhoneValidatorTests.cs" project="Gmtl.HandyLib.Tests" date="2025-12-15 17:22">
+// 
+// </copyright>
+// -------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Gmtl.HandyLib
+{
+    /// <summary>
+    /// Provides static methods for validating method arguments and ensuring they meet required conditions.
+    /// </summary>
+    /// <remarks>The HLAssert class contains utility methods for performing common argument validation checks,
+    /// such as verifying that parameters are not null or that strings are not empty or whitespace. These methods are
+    /// intended to be used at the start of methods to enforce preconditions and throw appropriate exceptions when
+    /// arguments are invalid. All methods are static and thread safe.</remarks>
+    public static class HLAssert
+    {
+        /// <summary>
+        /// Ensures string parameter is not null, empty, or whitespace. 
+        /// </summary>
+        public static void AssertArgument(string argument, string paramName)
+        {
+            if (paramName is null) throw new ArgumentNullException(nameof(paramName));
+            if (argument is null) throw new ArgumentNullException(paramName);
+            if (string.IsNullOrWhiteSpace(argument)) throw new ArgumentException("Argument cannot be empty or whitespace.", paramName);
+        }
+
+        /// <summary>
+        /// Ensures parameter is not null.
+        /// </summary>
+        public static void AssertArgument(object argument, string paramName)
+        {
+            if (paramName is null) throw new ArgumentNullException(nameof(paramName));
+            if (argument is null) throw new ArgumentNullException(paramName);
+        }
+    }
+}
